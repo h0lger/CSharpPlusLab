81275d4f016f8e56ed3a733fb3eeb4dc8d08a2cd
h0lger <audidave@gmail.com>
2014-05-04 09:19:14 +0200

    First comparison c++/c#

---
+++ CSharpPlusLab/MathTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlusLab
+{
+    public class MathTest
+    {
+
+        public double MyMethod(double a, double b, int no)
+        {
+            double z = 0;
+			for (int i = 0; i < no; i++)
+			{
+				z =+  Math.Sqrt((a+b+i) * 3.14);
+			}
+
+			return z;						
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        /// <summary>
+        /// C#
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            CPlusLab.MathTest t = new CPlusLab.MathTest();
+            long totTime = 0;
+            int iterations = 30;
+            double z = 0;
+            for (int i = 0; i < iterations; i++)
+            {
+                sw.Start();
+                z = t.MyMethod(5, 6, 10000000);
+                sw.Stop();
+                totTime += sw.ElapsedMilliseconds;
+                sw.Reset();
+            }
+            Console.WriteLine(z);
+            Console.WriteLine(totTime / iterations);
+            Console.ReadLine();
+        }
+
+        ///// <summary>
+        ///// C#
+        ///// </summary>
+        ///// <param name="args"></param>
+        //static void Main(string[] args)
+        //{
+        //    Stopwatch sw = new Stopwatch();
+        //    CSharpPlusLab.MathTest t = new CSharpPlusLab.MathTest();
+        //    long totTime = 0;
+        //    int iterations = 30;
+        //    double z = 0;
+        //    for (int i = 0; i < iterations; i++)
+        //    {
+        //        sw.Start();
+        //        z = t.MyMethod(5, 6, 10000000);
+        //        sw.Stop();
+        //        totTime += sw.ElapsedMilliseconds;
+        //        sw.Reset();
+        //    }            
+        //    Console.WriteLine(z);
+        //    Console.WriteLine(totTime / iterations);
+        //    Console.ReadLine();
+        //}
+    }
+}
